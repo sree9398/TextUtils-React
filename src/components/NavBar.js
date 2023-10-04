fb0a960fb175a5b31e287fe14ed5336f4eca13d5
Sreekanth Sree <cseaiml65@gmail.com>
2023-10-04 08:30:38 +0530

    changes on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { Link } from "react-router-dom";
 
-import {Link } from "react-router-dom";
 export default function NavBar(props) {
   const btnBox = {
     cursor: "pointer",
@@ -20,7 +19,7 @@ export default function NavBar(props) {
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="/navbarSupportedContent"
+          data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
@@ -53,31 +52,20 @@ export default function NavBar(props) {
               </div>
             </a>
             <ul className="dropdown-menu" style={btnBox}>
-              <li>
-                <li className="dropdown-item" style={{ backgroundColor: 'grey', borderRadius: "30px" }} onClick={() => props.toggleMode('grey')}>
-                  Dark
-                </li>
+              <li style={{ backgroundColor: 'grey', borderRadius: "30px" }} onClick={() => props.toggleMode('grey')}>
+                Dark
               </li>
-              <li>
-                <li className="dropdown-item" style={{ backgroundColor: 'white', borderRadius: "30px" }} onClick={() => props.toggleMode('white')}>
-                  Light
-                </li>
+              <li style={{ backgroundColor: 'white', borderRadius: "30px" }} onClick={() => props.toggleMode('white')}>
+                Light
               </li>
-
-              <li>
-                <li className="dropdown-item" style={{ backgroundColor: 'green', borderRadius: "30px" }} onClick={() => props.toggleMode('green')}>
-                  Green
-                </li>
+              <li style={{ backgroundColor: 'green', borderRadius: "30px" }} onClick={() => props.toggleMode('green')}>
+                Green
               </li>
-              <li>
-                <li className="dropdown-item" style={{ backgroundColor: 'violet', borderRadius: "30px" }} onClick={() => props.toggleMode('violet')}>
-                  Violet
-                </li>
+              <li style={{ backgroundColor: 'violet', borderRadius: "30px" }} onClick={() => props.toggleMode('violet')}>
+                Violet
               </li>
-              <li>
-                <li className="dropdown-item" style={{ backgroundColor: 'red', borderRadius: "30px" }} onClick={() => props.toggleMode('red')}>
-                  Red
-                </li>
+              <li style={{ backgroundColor: 'red', borderRadius: "30px" }} onClick={() => props.toggleMode('red')}>
+                Red
               </li>
             </ul>
           </li>
@@ -89,12 +77,10 @@ export default function NavBar(props) {
 
 NavBar.propTypes = {
   title: PropTypes.string,
-  home: PropTypes.string,
-  mode: PropTypes.string.isRequired, // Assuming 'mode' is required
-  toggleMode: PropTypes.func.isRequired, // Assuming 'toggleMode' is required
+  mode: PropTypes.string.isRequired,
+  toggleMode: PropTypes.func.isRequired,
 };
 
 NavBar.defaultProps = {
   title: "Sree",
-  home: "www.sree.com",
 };
